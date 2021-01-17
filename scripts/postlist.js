0fa9ec2b0c54b6a261dc8b6df7309d561ee282db
uiwarrior <arnab.sadhya@gmail.com>
2021-01-17 17:01:11 +0530

    adding redirection for post page

---
+++ scripts/postlist.js
@@ -0,0 +1,15 @@
+var deleteModalId = null;
+function deletePost(){
+   document.getElementById(deleteModalId).style.display = 'none';
+   $("#deleteConfirmationModal").modal("hide");
+}
+
+function openDeleteModal(_id)
+{   
+    deleteModalId = _id;
+    $("#deleteConfirmationModal").modal("show");
+}
+
+function openPostDetail(){
+    window.open("html/post.html",'_self')
+}
