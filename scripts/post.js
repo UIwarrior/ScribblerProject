3d10a7d8d5ca8a3c80f3cb16294973357b978dcc
uiwarrior <arnab.sadhya@gmail.com>
2021-01-18 20:54:36 +0530

    adding save logic, updated text show, include header

--- scripts/post.js
+++ scripts/post.js
@@ -1,4 +1,6 @@
+// likes count
 var likes = 0;
+//comments array
 var commentArray = [];
 
 
@@ -8,7 +10,7 @@ var commentArray = [];
 function triggerEditMode(){
 
     document.getElementById("contentEditableTextArea").value = document.getElementById("contentText").innerHTML;
-    document.getElementById("titleEditableInput").value = document.getElementById("postTitle").innerHTML;
+    document.getElementById("titleEditableInput").value = document.getElementById("postHeadingText").innerHTML;
     var editArray = document.getElementsByClassName('editMode');
     for(var i = 0; i< editArray.length; i++){
         editArray[i].style.display = 'block';
@@ -20,6 +22,8 @@ function triggerEditMode(){
 
 /**
  * This method is invoked when user clicks on like button;
+ * This increments initial like counter which is at first set to 0
+ * This also toggles the display of text when someone likes a post
  */
 
 function likePost(){
@@ -30,7 +34,9 @@ function likePost(){
  document.getElementById("likeText").innerHTML = 'Liked';
 }
 
-
+/**
+ * This method is used to post a comment from the input field and display that in the all comments section
+ */
 
 function postComment(){
 
@@ -48,9 +54,13 @@ function postComment(){
 }
 
 
+/**
+ * This function handles save logic when someone updated the content and heading.
+ */
+
 function savePost(){
-    document.getElementById("contentText").innerHTML =   document.getElementById("contentEditableTextArea").value;
-    document.getElementById("postTitle").innerHTML = document.getElementById("titleEditableInput").value;
+    document.getElementById("contentText").innerHTML =   '<p class="updatedText"><strong >Updated: </strong></p>' + document.getElementById("contentEditableTextArea").value;
+    document.getElementById("postHeadingText").innerHTML = '<strong class="updatedText">Updated: </strong>' + document.getElementById("titleEditableInput").value;
 
     var editArray = document.getElementsByClassName('editMode');
     for(var i = 0; i< editArray.length; i++){
@@ -59,4 +69,14 @@ function savePost(){
     for(var j = 0; j < document.getElementsByClassName("readMode").length; j++){
         document.getElementsByClassName("readMode")[j].style.display = 'block';
     }
+
+    // remove UPDATED text after 2 secs so that it does appear next time when we edit
+    setTimeout(function(){
+        var htmlCollectionLength = document.getElementsByClassName('updatedText').length;
+        for(var k =0; k <htmlCollectionLength; k++)
+        {
+            document.getElementsByClassName('updatedText')[0].remove();
+        }
+        
+    }, 2000)
 }
